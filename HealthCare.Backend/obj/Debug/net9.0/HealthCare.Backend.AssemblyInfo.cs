6b74162746c58ef093cc5b6e8e32c9bb15664632
Eugene Milford <gmilford321@gmail.com>
2025-04-13 15:08:15 +0200

    DB added

--- HealthCare.Backend/obj/Debug/net9.0/HealthCare.Backend.AssemblyInfo.cs
+++ HealthCare.Backend/obj/Debug/net9.0/HealthCare.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81c2441737edd1d5122939811f9e5e75f353fb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55ed081193e8aab84818203233f4a5634301f2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
