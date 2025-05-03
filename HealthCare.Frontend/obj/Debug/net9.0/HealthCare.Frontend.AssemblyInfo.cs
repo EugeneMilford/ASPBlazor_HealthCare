d5ac63ec588c0519cb798be3f04797c23584188c
Eugene Milford <gmilford321@gmail.com>
2025-05-03 16:45:10 +0200

    FrontEnd Razor Pages Completed

--- HealthCare.Frontend/obj/Debug/net9.0/HealthCare.Frontend.AssemblyInfo.cs
+++ HealthCare.Frontend/obj/Debug/net9.0/HealthCare.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67bf79f8cd34ea46495689c42104123b6a7d0a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f84e36fda9400e96835ced0d156bfd43eea203")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
